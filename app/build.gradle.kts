638bf774e2cfb5033d2ccba74c51a6a4f9bd1c98
Ratul Hasan <ratul.hasan.rahat.bd@gmail.com>
2026-08-27 09:38:21 +0600

    configure signing config and output file name

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,14 @@
+import com.android.build.api.variant.impl.VariantOutputImpl
+import java.io.FileInputStream
+import java.util.Properties
+
 plugins {
     alias(libs.plugins.android.application)
 }
+
+var keystorePropertiesFile: File = rootProject.file("app/keystore.properties")
+var keystoreProperties: Properties = Properties()
+keystoreProperties.load(FileInputStream(keystorePropertiesFile))
 
 android {
     namespace = "io.github.codehasan.developeroptions"
@@ -17,6 +25,15 @@ android {
         versionName = "1.0.1"
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = keystoreProperties["storeFile"]?.let { file(it) }
+            storePassword = keystoreProperties["storePassword"].toString()
+            keyAlias = keystoreProperties["keyAlias"].toString()
+            keyPassword = keystoreProperties["keyPassword"].toString()
+        }
+    }
+
     buildTypes {
         release {
             optimization {
@@ -28,6 +45,21 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
+    }
+
+    dependenciesInfo {
+        includeInApk = false
+        includeInBundle = false
+    }
+}
+
+androidComponents {
+    onVariants(selector().all()) { variant ->
+        variant.outputs.forEach { output ->
+            if (output is VariantOutputImpl) {
+                output.outputFileName.set("Developer_Options.apk")
+            }
+        }
     }
 }
 
